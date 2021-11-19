9f5faeb9fe1fa8669e7f40169838c03deb198654
Dulfrey Hernández <wdhernandezh@unal.edu.co>
2021-11-19 09:13:44 -0500

    logos

--- src/app/@theme/components/footer/footer.component.ts
+++ src/app/@theme/components/footer/footer.component.ts
@@ -7,9 +7,7 @@ import { Component } from '@angular/core';
     <span class="created-by">
       Created by <b><a href="https://www.it270.com" target="_blank">IT 270</a></b> 2021
     </span>
-    <img src="../assets/images/DNP.png">
-    <img src="../assets/images/DANE.png">
-    <img src="../assets/images/migracion.png">
+    
     
   `,
 })
