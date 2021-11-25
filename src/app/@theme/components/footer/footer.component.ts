c4121c833cea148508baad7fe2bb30bed3e9cd6a
Dulfrey Hernández <wdhernandezh@unal.edu.co>
2021-11-25 18:21:33 -0500

    logos gobierno

--- src/app/@theme/components/footer/footer.component.ts
+++ src/app/@theme/components/footer/footer.component.ts
@@ -7,7 +7,13 @@ import { Component } from '@angular/core';
     <span class="created-by">
       Created by <b><a href="https://www.it270.com" target="_blank">IT 270</a></b> 2021
     </span>
-    
+    <a class="logo" href="#" >
+      <img style="height: 40px; width: auto;" src="../../../../assets/images/logos/logo-unfpa-color.png" alt=""/>
+    </a>
+    <a class="logo" href="#" >
+      
+      <img style="height: 40px; width: 100%;" src="../../../../assets/images/logos/gobierno-colombia.png" alt=""/>
+    </a>
     
   `,
 })
